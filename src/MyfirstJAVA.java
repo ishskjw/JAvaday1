e42e64a9402d503595a7c4057e38bac3bee67e08
user <user@com14>
2017-10-24 20:59:00 +0900

    My second edit for java

--- src/MyfirstJAVA.java
+++ src/MyfirstJAVA.java
@@ -3,6 +3,14 @@ public class MyfirstJAVA {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 
 	}
 
